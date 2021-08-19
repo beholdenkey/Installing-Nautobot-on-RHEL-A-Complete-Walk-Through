68eca129a5192a8d603518c0692842a6819b4826
beholdenkey <justthered63@gmail.com>
2021-08-19 10:44:54 -0400

    lots of changes

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@
 # Script to install Nautobot -  RHEL 8
 # This process is being used to Create a Virtual Machine to then export as an OVA for Production Deployments.
 #------------------------------------------------------------- #
-# Resources - 16 vCores 32 GiB RAM 250 GiB Storage
 # Leave the Host Name as the default (localhost.localdomain) this can be changed as needed later.
 # It is important to remember that you need to remove all Network Configurations prior to exporting as an OVA
 # The DISA STIG for Red Hat Enterprise LInux - Will be applied so be sure to correctly partition the storage
