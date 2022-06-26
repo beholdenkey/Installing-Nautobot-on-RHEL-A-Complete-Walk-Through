1f2fcd18d4ea8faa6fba9c12ec750cc2c2d9d8f9
B3H0LD3NK3Y <44618918+beholdenkey@users.noreply.github.com>
2022-06-26 14:00:28 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -104,14 +104,17 @@ install_dependencies() {
 install_postgresql() {
     if [ "$OS" = "Red Hat Enterprise Linux" ]; then
         if [ "$VER" = "8.0" ]; then
-            sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-            sudo dnf install -y postgresql14-server
-            sudo /usr/pgsql-14/bin/postgresql-14-setup initdb
-            sudo systemctl enable --now postgresql-14
+            #sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+            #sudo dnf install -y postgresql14-server
+            dnf -y module install postgresql:14/server
+            postgresql-setup --initdb
+            sudo systemctl enable --now postgresql
         elif [ "$VER" = "9.0" ]; then
-            sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-            sudo /usr/pgsql-14/bin/postgresql-14-setup initdb
-            sudo systemctl enable --now postgresql-14
+            #sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+            #sudo dnf install -y postgresql14-server
+            dnf -y module install postgresql:14/server
+            postgresql-setup --initdb
+            sudo systemctl enable --now postgresql
         else
             echo "Unsupported OS Version: $VER"
         fi
@@ -119,8 +122,8 @@ install_postgresql() {
         if [ "$VER" = "36.0" ]; then
             dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/F-36-x86_64/pgdg-fedora-repo-latest.noarch.rpm
             dnf install -y postgresql14-server
-            sudo /usr/pgsql-14/bin/postgresql-14-setup initdb
-            sudo systemctl enable --now postgresql-14
+            postgresql-setup --initdb
+            sudo systemctl enable --now postgresql
         else
             echo "Unsupported OS Version: $VER"
         fi
@@ -137,7 +140,7 @@ configure_postgres_conf() {
         elif [ "$VER" = "9.0" ]; then
             sudo cp /etc/postgresql/14/main/postgresql.conf /etc/postgresql/14/main/postgresql.conf.bak
             sudo sed -i 's/#password_encryption = on/password_encryption = scram-sha-256/g' /etc/postgresql/14/main/postgresql.conf
-            sudo systemctl restart postgresql-14
+            sudo systemctl restart postgresql
         else
             echo "Unsupported OS Version: $VER"
         fi
@@ -145,7 +148,7 @@ configure_postgres_conf() {
         if [ "$VER" = "36.0" ]; then
             sudo cp /etc/postgresql/14/main/postgresql.conf /etc/postgresql/14/main/postgresql.conf.bak
             sudo sed -i 's/#password_encryption = on/password_encryption = scram-sha-256/g' /etc/postgresql/14/main/postgresql.conf
-            sudo systemctl restart postgresql-14
+            sudo systemctl restart postgresql
         else
             echo "Unsupported OS Version: $VER"
         fi
@@ -165,7 +168,7 @@ check_pghba() {
             sudo cp /etc/postgresql/14/main/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf.bak
             sudo sed -i 's/local   all             all                                     peer/local   all             all                                     scram-256/g' /etc/postgresql/14/main/pg_hba.conf
             sudo sed -i 's/host    all             all                                                                                                          scram-256/g' /ect/postgresql/14/main/pg_hba/conf
-            sudo systemctl restart postgresql-14
+            sudo systemctl restart postgresql
         else
             echo "Unsupported OS Version: $VER"
         fi
@@ -174,7 +177,7 @@ check_pghba() {
             sudo cp /etc/postgresql/14/main/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf.bak
             sudo sed -i 's/local   all             all                                     peer/local   all             all                                     scram-256/g' /etc/postgresql/14/main/pg_hba.conf
             sudo sed -i 's/host    all             all                                                                                                          scram-256/g' /ect/postgresql/14/main/pg_hba/conf
-            sudo systemctl restart postgresql-14
+            sudo systemctl restart postgresql
         fi
     else
         echo "Unsupported OS: $OS"
@@ -184,14 +187,14 @@ check_pghba() {
 configure_postgresql_db() {
     if [ "$OS" = "Red Hat Enterprise Linux" ]; then
         if [ "$VER" = "8.0" ]; then
-            sudo systemctl start postgresql-14
-            sudo systemctl enable postgresql-14
+            sudo systemctl start postgresql
+            sudo systemctl enable postgresql
             sudo -u postgres psql -c "CREATE USER nautobot WITH PASSWORD 'P@ssw0rd12';"
             sudo -u postgres psql -c "CREATE DATABASE nautobot;"
             sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE nautobot TO nautobot;"
         elif [ "$VER" = "9.0" ]; then
-            sudo systemctl start postgresql-14
-            sudo systemctl enable postgresql-14
+            sudo systemctl start postgresql
+            sudo systemctl enable postgresql
             sudo -u postgres psql -c "CREATE USER nautobot WITH PASSWORD 'P@ssw0rd12';"
             sudo -u postgres psql -c "CREATE DATABASE nautobot;"
             sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE nautobot TO nautobot;"
@@ -200,8 +203,8 @@ configure_postgresql_db() {
         fi
     elif [ "$OS" = "Fedora" ]; then
         if [ "$VER" = "36.0" ]; then
-            sudo systemctl start postgresql-14
-            sudo systemctl enable postgresql-14
+            sudo systemctl start postgresql
+            sudo systemctl enable postgresql
             sudo -u postgres psql -c "CREATE USER nautobot WITH PASSWORD 'P@ssw0rd12';"
             sudo -u postgres psql -c "CREATE DATABASE nautobot;"
             sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE nautobot TO nautobot;"
